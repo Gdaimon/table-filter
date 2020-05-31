cd5e9118097e664ccf775728cc6ebf32d44e3f45
Carlos Andres <darkklitos@gmail.com>
2020-05-30 19:21:28 -0500

    [add] Se cargan los datos a la tabla

--- app.js
+++ app.js
@@ -47,8 +47,24 @@
     },
   ]
 
-  console.log(datos);
+  function cargarTabla(data) {
+    const table = document.getElementById('table-data')
 
+    for (let [index, user] of data.entries()) {
+      const row = `
+      <tr class='text-center hover:bg-blue-500'>
+        <td class="py-4 px-6 border-b border-grey-light">${index + 1}</td>
+        <td class="py-4 px-6 border-b border-grey-light">${user.first_name}</td>
+        <td class="py-4 px-6 border-b border-grey-light">${user.last_name}</td>
+        <td class="py-4 px-6 border-b border-grey-light">${user.email}</td>
+      </tr>
+      `
+      table.innerHTML += row;
+    }
+
+  }
+
+  cargarTabla(datos)
 
 
 })();
