9854003580ceb53e7d0aef935d7c721591301099
Carlos Andres <darkklitos@gmail.com>
2020-05-30 20:24:08 -0500

    [add] Se implementa filtro desde el input

--- app.js
+++ app.js
@@ -4,12 +4,40 @@
 
   let datos = []
 
+  const $input = document.getElementById('input-filtro')
+  $input.addEventListener('keyup', (event) => {
+    filtrarDatos(event.target.value, [...datos])
+  })
+
+  function filtrarDatos(parametro, datos) {
+    const palabra = parametro.trim();
+    if (palabra) {
+      let dataFiltrada = datos.filter((user) => buscarEnObjeto(user, palabra))
+      cargarTabla(dataFiltrada);
+    } else {
+      cargarTabla(datos)
+    }
+  }
+
+  function buscarEnObjeto(objecto, palabra) {
+    for (let key in objecto) {
+      if (objecto.hasOwnProperty(key)) {
+        if (objecto[key]) {
+          if (objecto[key].toString().toLowerCase().includes(palabra)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
   function cargarTabla(data) {
     const table = document.getElementById('table-data')
-
+    table.innerHTML = ''
     for (let [index, user] of data.entries()) {
       const row = `
-      <tr class='text-center hover:bg-blue-500'>
+      <tr class='text-center hover:bg-blue-100'>
         <td class="py-4 px-6 border-b border-grey-light">${index + 1}</td>
         <td class="py-4 px-6 border-b border-grey-light">${user.first_name}</td>
         <td class="py-4 px-6 border-b border-grey-light">${user.last_name}</td>
@@ -18,18 +46,22 @@
       `
       table.innerHTML += row;
     }
-
   }
 
   async function consultarDatos() {
     const URL = "https://reqres.in/api/users"
-    const response = await fetch(URL);
-    const responseData = await response.json();
-    cargarTabla(responseData.data)
-
+    try {
+      const response = await fetch(URL);
+      const responseData = await response.json();
+      return new Promise((resolve, reject) => {
+        resolve(responseData.data)
+      })
+    } catch (e) {
+      return new Promise((resolve, reject) => reject(e))
+    }
   }
 
-  consultarDatos()
-
+  datos = await consultarDatos()
+  cargarTabla(datos)
 
 })();
